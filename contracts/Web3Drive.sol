e0b8f5acaae2ff1ec0e7bb9934592b2cb053934a
Ashutosh Jha <ashutosh26jha@gmail.com>
2023-03-31 11:59:06 +0530

    Structure of contract

--- contracts/Web3Drive.sol
+++ contracts/Web3Drive.sol
@@ -1,5 +1,8 @@
 pragma solidity ^0.8.7;
 // SPDX-License-Identifier: MIT
 contract Web3Drive{
-    
+    mapping (address => mapping (string => string)) private s_ownfiles;
+    mapping (address => mapping (address => string)) private s_nicknames;
+    mapping (string => address [] ) private s_hasAccess;
+
 }
